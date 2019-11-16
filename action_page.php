45e56b065738bfd1bd705e691a1b5d8f001da989
Gemma <gemmadruce@live.co.uk>
2019-11-16 13:12:00 +0000

    contact take 2

--- action_page.php
+++ action_page.php
@@ -1,53 +1,17 @@
 <?php
-if($_POST["message"]) {
-    mail("gemmadruce@live.co.uk", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if (isset($_POST['submit'])) {
+    $firstname = $['firstname'];
+    $lastname = $['lastname'];
+    $mailFrom = $['email'];
+    $message = $['message'];
+}
+
+$mailTo = "gemmadruce@live.co.uk";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an email from ".$firstname. ".\n\n".$message; 
+
+mail($mailTo,$subject, $txt, $headers);
+header("Location: index.php?mailsend");
 }
 ?>
-
-<?php
-
-if($_POST["submit"]) {
-    $recipient="your@email.address";
-    $subject="Form to email message";
-    $sender=$_POST["fname"];
-    $senderEmail=$_POST["email"];
-    $message=$_POST["message"];
-
-    $mailBody="Name: $sender\nEmail: $email\n\n$subject";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
-
-?><!DOCTYPE html>
-
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Contact form to email</title>
-</head>
-
-<body>
-
-    <?=$thankYou ?>
-
-    <form method="post" action="contact.php">
-        <label>First name:</label>
-        <input name="fname">
-
-        <label>Last name:</label>
-        <input name="lname">
-
-        <label>Email address:</label>
-        <input name="email">
-
-        <label>Message:</label>
-        <textarea rows="5" cols="20" name="subject"></textarea>
-
-        <input type="submit" name="submit">
-    </form>
-
-</body>
-
-</html>
